f33560a40ce368893fb078b2bdd85a4012d31868
Mihail Tanev <mihail.v.tanev@gmail.com>
2019-03-09 12:27:33 +0100

    added products

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Sony GD-380",
+    img: "img/1.jpeg",
+    price: 10,
+    company: "Sony",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Nikon D-900",
+    img: "img/2.jpeg",
+    price: 10,
+    company: "Nikon",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Sony WD-670",
+    img: "img/3.jpeg",
+    price: 10,
+    company: "Sony",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Konica Minolta",
+    img: "img/4.jpeg",
+    price: 10,
+    company: "Canon",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Canon l-200",
+    img: "img/5.jpeg",
+    price: 10,
+    company: "Canon",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Canon HL-330",
+    img: "img/6.jpg",
+    price: 10,
+    company: "Canon",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Nikon D-350",
+    img: "img/7.jpeg",
+    price: 10,
+    company: "Nikon",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Leika U-900",
+    img: "img/8.jpeg",
+    price: 10,
+    company: "Leika",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi beatae dolorum, aut aperiam repudiandae veritatis mollitia rerum, maiores similique, eaque vero. Ullam illum aliquid quia omnis molestias dolorum. Ipsa, assumenda! Eligendi tenetur totam unde reiciendis sed aut rerum, possimus soluta corrupti alias iste similique quia veritatis, voluptas non culpa facilis.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google",
+  img: "img/1.jpeg",
+  price: 25,
+  company: "google",
+  info:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis earum nesciunt tempora repudiandae, necessitatibus fugit id odit nulla. Perferendis sit vero a aliquam quod repellat ab modi hic illum odio? Quasi placeat voluptatem repellat ipsa perspiciatis quod ratione quibusdam, soluta minima facilis omnis ducimus repellendus at illo corrupti itaque. Neque.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
