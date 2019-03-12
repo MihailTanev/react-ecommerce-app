91847835387fdaf8278488deeb3be610675dda24
Mihail Tanev <mihail.v.tanev@gmail.com>
2019-03-12 12:44:51 +0100

    cart added

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const storeProducts = [
     id: 1,
     title: "Sony GD-380",
     img: "img/1.jpeg",
-    price: 589.99,
+    price: 589,
     company: "Sony",
     info:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quibusdam sunt quia commodi facere nulla, ab, deleniti dolorum ipsum voluptatum consequatur. Nam blanditiis natus voluptates fugit dolores, a sequi commodi, facilis quibusdam temporibus rerum nemo!",
@@ -15,7 +15,7 @@ export const storeProducts = [
     id: 2,
     title: "Nikon D-900",
     img: "img/2.jpeg",
-    price: 779.99,
+    price: 779,
     company: "Nikon",
     info:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quibusdam sunt quia commodi facere nulla, ab, deleniti dolorum ipsum voluptatum consequatur. Nam blanditiis natus voluptates fugit dolores, a sequi commodi, facilis quibusdam temporibus rerum nemo!",
@@ -27,7 +27,7 @@ export const storeProducts = [
     id: 3,
     title: "Sony WD-670",
     img: "img/3.jpeg",
-    price: 899.99,
+    price: 899,
     company: "Sony",
     info:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quibusdam sunt quia commodi facere nulla, ab, deleniti dolorum ipsum voluptatum consequatur. Nam blanditiis natus voluptates fugit dolores, a sequi commodi, facilis quibusdam temporibus rerum nemo!",
@@ -39,7 +39,7 @@ export const storeProducts = [
     id: 4,
     title: "Konica Minolta",
     img: "img/4.jpeg",
-    price: 499.99,
+    price: 499,
     company: "Canon",
     info:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quibusdam sunt quia commodi facere nulla, ab, deleniti dolorum ipsum voluptatum consequatur. Nam blanditiis natus voluptates fugit dolores, a sequi commodi, facilis quibusdam temporibus rerum nemo!",
@@ -51,7 +51,7 @@ export const storeProducts = [
     id: 5,
     title: "Canon l-200",
     img: "img/5.jpeg",
-    price: 399.99,
+    price: 399,
     company: "Canon",
     info:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quibusdam sunt quia commodi facere nulla, ab, deleniti dolorum ipsum voluptatum consequatur. Nam blanditiis natus voluptates fugit dolores, a sequi commodi, facilis quibusdam temporibus rerum nemo!",
@@ -63,7 +63,7 @@ export const storeProducts = [
     id: 6,
     title: "Canon HL-330",
     img: "img/6.jpg",
-    price: 679.99,
+    price: 679,
     company: "Canon",
     info:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quibusdam sunt quia commodi facere nulla, ab, deleniti dolorum ipsum voluptatum consequatur. Nam blanditiis natus voluptates fugit dolores, a sequi commodi, facilis quibusdam temporibus rerum nemo!",
@@ -75,7 +75,7 @@ export const storeProducts = [
     id: 7,
     title: "Nikon D-350",
     img: "img/7.jpeg",
-    price: 689.99,
+    price: 689,
     company: "Nikon",
     info:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quibusdam sunt quia commodi facere nulla, ab, deleniti dolorum ipsum voluptatum consequatur. Nam blanditiis natus voluptates fugit dolores, a sequi commodi, facilis quibusdam temporibus rerum nemo!",
@@ -102,7 +102,7 @@ export const detailProduct = {
   id: 1,
   title: "Google",
   img: "img/1.jpeg",
-  price: 199.99,
+  price: 199,
   company: "google",
   info:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis earum nesciunt tempora repudiandae, necessitatibus fugit id odit nulla. Perferendis sit vero a aliquam quod repellat ab modi hic illum odio? Quasi placeat voluptatem repellat ipsa perspiciatis quod ratione quibusdam, soluta minima facilis omnis ducimus repellendus at illo corrupti itaque. Neque.",
